Exozodi scaling bug fixed
Had the distance scaling for the exozodi flux flipped, so the zodi level was off by a factor of 16.  Oops.

hcihrsreduction.py
@@ -47,10 +47,11 @@ class HCI_HRS_Reduction():
             self.obs_st_at_removed = self.removeSkyTransmissionStar()            
             #obs_norm = self.obs_st_at_removed.getSpecNorm(num_chunks=20, poly_order=3)
             print(str(datetime.now()))
-            #obs_norm = self.getStarNorm(self.hci_hrs_obs.obs_st_resample.flux, long_array=True)
-            #obs_norm = obs_norm.value / np.median(obs_norm.value) * np.median(self.obs_st_at_removed.flux)
-            obs_norm = np.median(self.obs_st_at_removed.flux) #Normalization is to make sure that the input spectrum for the CCF is flat; 
+            #Normalization is to make sure that the input spectrum for the CCF is flat; 
             #this is especially important for large wavelength coverage.
+            obs_norm = self.getStarNorm(self.hci_hrs_obs.obs_st_resample.flux, long_array=True)
+            obs_norm = obs_norm.value / np.median(obs_norm.value) * np.median(self.obs_st_at_removed.flux)
+            #obs_norm = np.median(self.obs_st_at_removed.flux) 
             self.obs_st_at_removed.flux = self.obs_st_at_removed.flux / obs_norm
             self.obs_st_at_removed.noise = self.obs_st_at_removed.noise / obs_norm
             #mask_arr = np.where((self.template_resample.flux / np.nanmedian(self.template_resample.flux)) > 0.99)